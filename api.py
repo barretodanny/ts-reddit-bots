9e59cad2cd79bb668bd6195d60371639a1b15379
barretodanny <dannybarreto22@gmail.com>
2023-12-13 14:39:31 -0500

    create post api function

--- api.py
+++ api.py
@@ -3,9 +3,14 @@ import requests
 ENDPOINT = "https://ts-reddit-rest-api.onrender.com/api"
 LOGIN_ENDPOINT = f"{ENDPOINT}/sessions"
 SUBREDDITS_ENDPOINT = f"{ENDPOINT}/subreddits"
+POSTS_ENDPOINT = f"{ENDPOINT}/posts"
 
 def create_session(payload):
     return requests.post(LOGIN_ENDPOINT, json=payload)
 
 def get_subreddit_by_name(subreddit_name):
     return requests.get(f"{SUBREDDITS_ENDPOINT}/{subreddit_name}?string=true")
+
+def create_post(payload, token):
+    header = {"Authorization": f"Bearer {token}"}
+    return requests.post(f"{POSTS_ENDPOINT}", json=payload, headers=header)
