9b3e1c68fe2ae50ee014b561b0d47ebfade65527
barretodanny <dannybarreto22@gmail.com>
2023-12-13 14:39:09 -0500

    get subreddit by name api function

--- api.py
+++ api.py
@@ -2,6 +2,10 @@ import requests
 
 ENDPOINT = "https://ts-reddit-rest-api.onrender.com/api"
 LOGIN_ENDPOINT = f"{ENDPOINT}/sessions"
+SUBREDDITS_ENDPOINT = f"{ENDPOINT}/subreddits"
 
 def create_session(payload):
     return requests.post(LOGIN_ENDPOINT, json=payload)
+
+def get_subreddit_by_name(subreddit_name):
+    return requests.get(f"{SUBREDDITS_ENDPOINT}/{subreddit_name}?string=true")
