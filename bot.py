7c7832d22448bc854c3148cccf86034dafa4ad0d
barretodanny <dannybarreto22@gmail.com>
2023-12-13 14:38:35 -0500

    bot function to create a post on a subreddit

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from api import create_session
+from api import *
 import json
 
 # takes in login credentials and returns tokens or -1 is log in failed
@@ -14,3 +14,22 @@ def login(email, password):
     data = json.loads(req.text)
     return data
 
+def send_post(title, content, subreddit_name, token):
+    req = get_subreddit_by_name(subreddit_name)
+
+    # subreddit not found
+    if (req.status_code != 200):
+        print(f"Error fetching {subreddit_name}. Exiting.")
+        exit()
+    
+    subreddit = json.loads(req.text)
+    subreddit_id = subreddit["_id"]
+
+    post_payload = {"title": title, "content": content, "subreddit": subreddit_id}
+    req = create_post(post_payload, token)
+
+    # failed to create post
+    if (req.status_code != 201):
+        print("Failed to create post.")
+        return
+    
