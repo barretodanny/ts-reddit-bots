7a4395258d38d905b0caebb9cd359a87bd397401
barretodanny <dannybarreto22@gmail.com>
2023-12-13 15:31:08 -0500

    formatter

--- api.py
+++ api.py
@@ -5,11 +5,14 @@ LOGIN_ENDPOINT = f"{ENDPOINT}/sessions"
 SUBREDDITS_ENDPOINT = f"{ENDPOINT}/subreddits"
 POSTS_ENDPOINT = f"{ENDPOINT}/posts"
 
+
 def create_session(payload):
     return requests.post(LOGIN_ENDPOINT, json=payload)
 
+
 def get_subreddit_by_name(subreddit_name):
     return requests.get(f"{SUBREDDITS_ENDPOINT}/{subreddit_name}?string=true")
+
 
 def create_post(payload, token):
     header = {"Authorization": f"Bearer {token}"}
--- bot.py
+++ bot.py
@@ -1,27 +1,29 @@
 from api import *
 import json
 
+
 # takes in login credentials and returns tokens or -1 is log in failed
 def login(email, password):
     login_payload = {"email": email, "password": password}
     req = create_session(login_payload)
 
     # failed to log in
-    if (req.status_code != 201):
+    if req.status_code != 201:
         print("Failed to log in bot. Exiting.")
         exit()
-    
+
     data = json.loads(req.text)
     return data
+
 
 def send_post(title, content, subreddit_name, token):
     req = get_subreddit_by_name(subreddit_name)
 
     # subreddit not found
-    if (req.status_code != 200):
+    if req.status_code != 200:
         print(f"Error fetching {subreddit_name}. Exiting.")
         exit()
-    
+
     subreddit = json.loads(req.text)
     subreddit_id = subreddit["_id"]
 
@@ -29,7 +31,6 @@ def send_post(title, content, subreddit_name, token):
     req = create_post(post_payload, token)
 
     # failed to create post
-    if (req.status_code != 201):
+    if req.status_code != 201:
         print("Failed to create post.")
         return
-    
