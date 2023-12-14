d86da30f18b878434cf19f950fe0d4321f1e911a
barretodanny <dannybarreto22@gmail.com>
2023-12-14 17:54:19 -0500

    weather api endpoint, key, and get weather for city function

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 import requests
+from config import read_config
 
+##### TS-REDDIT API #####
 ENDPOINT = "https://ts-reddit-rest-api.onrender.com/api"
 LOGIN_ENDPOINT = f"{ENDPOINT}/sessions"
 SUBREDDITS_ENDPOINT = f"{ENDPOINT}/subreddits"
@@ -17,3 +19,13 @@ def get_subreddit_by_name(subreddit_name):
 def create_post(payload, token):
     header = {"Authorization": f"Bearer {token}"}
     return requests.post(f"{POSTS_ENDPOINT}", json=payload, headers=header)
+
+
+##### WEATHER API #####
+weather_api_profile = read_config("config.ini", "weather_api")
+WEATHER_ENDPOINT = weather_api_profile["url"]
+WEATHER_API_KEY = weather_api_profile["key"]
+
+
+def get_city_weather(city):
+    return requests.get(f"{WEATHER_ENDPOINT}{city}&APPID={WEATHER_API_KEY}")
